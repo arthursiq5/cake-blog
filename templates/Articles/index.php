4ff3ed1dad07cc50af9b24494b4cd8783d6122b0
Arthur Siqueira <arthur@oneweb.com.br>
2021-02-04 10:51:32 -0300

    Adicionada funcionalidade de atualizar artigos

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -7,17 +7,18 @@
         <th>Created</th>
     </tr>
 
-    <!-- Aqui é onde iremos iterar nosso objeto de solicitação $articles, exibindo informações de artigos -->
-
     <?php foreach ($articles as $article): ?>
-    <tr>
-        <td><?= $article->id ?></td>
-        <td>
-            <?= $this->Html->link($article->title, ['action' => 'view', $article->id]) ?>
-        </td>
-        <td>
-            <?= $article->created->format(DATE_RFC850) ?>
-        </td>
-    </tr>
+        <tr>
+            <td><?= $article->id ?></td>
+            <td>
+                <?= $this->Html->link($article->title, ['action' => 'view', $article->id]) ?>
+            </td>
+            <td>
+                <?= $article->created->format(DATE_RFC850) ?>
+            </td>
+            <td>
+                <?= $this->Html->link('Editar', ['action' => 'edit', $article->id]) ?>
+            </td>
+        </tr>
     <?php endforeach; ?>
 </table>
