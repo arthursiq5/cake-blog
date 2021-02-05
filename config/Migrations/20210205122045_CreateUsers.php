24e02f477fb4a6e62f30c7d0d104c31baf77af3d
Arthur Siqueira <arthur@oneweb.com.br>
2021-02-05 10:40:17 -0300

    Criada migration para adicionar tabela de usuários

---
+++ config/Migrations/20210205122045_CreateUsers.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('role', 'string', [
+            'default' => null,
+            'limit' => 20,
+            'null' => true,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->create();
+    }
+}
