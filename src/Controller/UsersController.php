162e1d4b80e8a800bd80537b2944c8e06f004ea0
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-25 15:54:51 -0300

    adicionada tela de criação de usuário

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Event\EventInterface;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UsersController extends AppController
+{
+    public function beforeFilter(EventInterface $event): void
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow('add');
+    }
+
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Auth');
+    }
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $this->set('users', $this->Users->find('all'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $user = $this->Users->get($id);
+
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $user = $this->Users->newEntity([]);
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('O usuário foi salvo.'));
+                return $this->redirect(['action' => 'add']);
+            }
+            $this->Flash->error(__('Não é possível adicionar o usuário.'));
+        }
+        $this->set('user', $user);
+    }
+}
